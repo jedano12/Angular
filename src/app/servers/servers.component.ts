c10f12c2eeb4789146fec8bd52ff8405b8a34c9b
jedano12 <jomar.edano12@gmail.com>
2023-10-21 19:40:04 +0800

    Databinding: String interpolation, property binding, event binding, two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Initial Value';
+  userName = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!' + this.serverName; 
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
